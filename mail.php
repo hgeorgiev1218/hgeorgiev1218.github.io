689d2a92ce95c9605a57654bd769c496f80dafbb
hgeorgiev1218 <hgeorgiev1218@gmail.com>
2023-04-01 23:01:29 +0300

    xcv

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+if(isset( $_POST['name']))
+$name = $_POST['name'];
+if(isset( $_POST['email']))
+$email = $_POST['email'];
+if(isset( $_POST['message']))
+$message = $_POST['message'];
+if(isset( $_POST['phone_no']))
+$phone_no = $_POST['phone_no'];
+
+$content="From: $name \n Email: $email \n Message: $message";
+$recipient = "hgeorgiev1218@gmail.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $phone_no, $content, $mailheader) or die("Error!");
+echo "Вашето съобщение е изпратено. Очаквайте да се свържем с вас!";
+?>
